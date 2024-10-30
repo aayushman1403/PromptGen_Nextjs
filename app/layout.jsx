86f5785c00869681e732ba1dd84055d01d87fd3d
aayushman1403 <aayushman.dash22@spit.ac.in>
2024-10-31 01:53:19 +0530

    first commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,8 +4,8 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-  title: "Promptopia",
-  description: "Discover & Share AI Prompts",
+  title: "Prompt.ai",
+  description: "Explore & Share AI Prompts",
 };
 
 const RootLayout = ({ children }) => (
